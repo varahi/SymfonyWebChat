b8253cda9a3c769ce417ad3338895a3c7751f1f1
Dmitry Vasiliev <d.vasiliev@connecta.ag>
2025-12-01 23:00:06 +0300

    feat. - added security to admin panel

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,17 +16,20 @@ class SecurityController extends AbstractController
         //     return $this->redirectToRoute('target_path');
         // }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $authenticationUtils->getLastAuthenticationError(),
+            ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // Symfony will handle it itself
+        // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
